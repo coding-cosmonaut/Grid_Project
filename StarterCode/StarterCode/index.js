4059e2f2ce5f6c119d2d1aa4fbfec79d6f6d005f
Alex Morgun <aamorgun2003@gmail.com>
2023-09-29 20:35:38 -0600

    Fifth added animations for hamburger button: open and close using JS; fixed some GRID issues;

--- StarterCode/StarterCode/index.js
+++ StarterCode/StarterCode/index.js
@@ -1,12 +1,42 @@
-const hamburgerBttn = document.querySelector('.hamburger')
-const hamburgerBttn1 = document.querySelector('.bar1')
-const hamburgerBttn2 = document.querySelector(".bar2");
-const hamburgerBttn3 = document.querySelector(".bar3");
-const mobileDrawer = document.querySelector(".mobileNav");
+const hamburgerBttn = document.querySelector(".hamburger");
+const allBars = document.querySelectorAll("#bar1, #bar2, #bar3");
+// const mobileDrawer = document.querySelector(".mobile-nav");
 
-hamburgerBttn.addEventListener('click', () => {
-    hamburgerBttn1.classList.toggle('anBar1');
-    hamburgerBttn2.classList.toggle('anBar2');
-    hamburgerBttn3.classList.toggle('anBar3');
-    mobileDrawer.classList.toggle("mobileNavAn");
-})
+hamburgerBttn.addEventListener("click", () => {
+  let [firstLine, secondLine, thirdLine] = allBars;
+  for (let bar of allBars) {
+    if (bar === firstLine) {
+      if (bar.classList.contains("closeAnimation1")) {
+        bar.classList.remove("closeAnimation1");
+      }
+      if (!bar.classList.contains("line1")) {
+        bar.classList.add("line1");
+      } else {
+        bar.classList.remove("line1");
+        bar.classList.add("closeAnimation1");
+      }
+    }
+    if (bar === secondLine) {
+      if (bar.classList.contains("closeAnimation2")) {
+        bar.classList.remove("closeAnimation2");
+      }
+      if (!bar.classList.contains("line2")) {
+        bar.classList.add("line2");
+      } else {
+        bar.classList.remove("line2");
+        bar.classList.add("closeAnimation2");
+      }
+    }
+    if (bar === thirdLine) {
+      if (bar.classList.contains("closeAnimation3")) {
+        bar.classList.remove("closeAnimation3");
+      }
+      if (!bar.classList.contains("line3")) {
+        bar.classList.add("line3");
+      } else {
+        bar.classList.remove("line3");
+        bar.classList.add("closeAnimation3");
+      }
+    }
+  }
+});
